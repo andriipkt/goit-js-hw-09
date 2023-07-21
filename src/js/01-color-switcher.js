5b0380174ee603c9365c7cbe8e8eaf07e0ec73cc
andriipkt <spinnevski@ukr.net>
2023-07-21 10:59:24 -0700

    finished 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const body = document.body;
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+////
 
+// functions
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function colorSwitcher() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+}
+////
+
+let colorInterval;
+
+// buttons
+startButton.addEventListener('click', () => {
+  startButton.classList.add('active');
+  stopButton.classList.remove('active');
+
+  colorInterval = setInterval(colorSwitcher, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  stopButton.classList.add('active');
+  startButton.classList.remove('active');
+
+  clearInterval(colorInterval);
+});
+////
---
+++ src/js/02-timer.js
@@ -0,0 +1,42 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+// const calendar = document.querySelector('#datetime-picker');
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysElem = document.querySelector('[data-days]');
+const hoursElem = document.querySelector('[data-hours]');
+const minutesElem = document.querySelector('[data-minutes]');
+const secondsElem = document.querySelector('[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+// functions
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+//
+flatpickr(datetimePicker, options);
